c1b5d5a0254a22ebc19762000f88bd3811e47ba8
tanya26romi <tanyatwinkles26@gmail.com>
2025-06-22 00:49:07 +0530

    Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header";
 import WelcomeSection from "./components/WelcomeSection";
@@ -14,8 +11,6 @@ import ContactDetails from "./components/ContactDetails";
 import GallerySection from "./components/GallerySection";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Router>
       <Routes>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 // Reference: https://tailwindui.com/components/application-ui/navigation/navbars
 
